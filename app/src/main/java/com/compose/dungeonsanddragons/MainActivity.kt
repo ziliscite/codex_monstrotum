75809106ee83e74d769c51057a7465069d87f19d
manzil.akbar@gmail.com <manzil.akbar@gmail.com>
2024-10-17 16:20:07 +0700

    Added splash screen

--- app/src/main/java/com/compose/dungeonsanddragons/MainActivity.kt
+++ app/src/main/java/com/compose/dungeonsanddragons/MainActivity.kt
@@ -12,11 +12,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.compose.dungeonsanddragons.ui.theme.DungeonsAndDragonsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             DungeonsAndDragonsTheme {
