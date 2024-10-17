52c4b2c31b8f7749bfac8d3a7c3291674ecd17ca
manzil.akbar@gmail.com <manzil.akbar@gmail.com>
2024-10-17 15:51:54 +0700

    First commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,101 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.compose.dungeonsanddragons"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.compose.dungeonsanddragons"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(platform(libs.jetbrains.kotlin.bom))
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    //Splash Api
+    implementation(libs.androidx.core.splashscreen)
+
+    //Compose Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    //Compose Foundation
+    implementation(libs.androidx.foundation)
+
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    //Coil
+    implementation(libs.coil.compose)
+
+    //Datastore
+    implementation(libs.androidx.datastore.preferences)
+
+    //Room
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+
+    //Paging 3
+    implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.androidx.paging.compose)
+
+    //Accompanist
+    implementation(libs.accompanist.systemuicontroller)
+
+    //Dagger Hilt
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+}
---
+++ app/src/main/java/com/compose/dungeonsanddragons/ui/theme/Type.kt
@@ -0,0 +1,53 @@
+package com.compose.dungeonsanddragons.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.compose.dungeonsanddragons.R
+
+val Caudex = FontFamily(
+    fonts = listOf(
+        Font(R.font.caudex_regular, FontWeight.Normal),
+        Font(R.font.caudex_bold, FontWeight.Bold),
+        Font(R.font.caudex_italic, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.caudex_bold_italic, FontWeight.Bold, FontStyle.Italic)
+    )
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    displaySmall = TextStyle(
+        fontSize = 24.sp,
+        fontFamily = Caudex,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 36.sp,
+    ),
+    displayMedium = TextStyle(
+        fontSize = 32.sp,
+        fontFamily = Caudex,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 48.sp,
+    ),
+    bodySmall = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = Caudex,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 21.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = Caudex,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 24.sp,
+    ),
+    labelSmall = TextStyle(
+        fontSize = 13.sp,
+        fontFamily = Caudex,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 19.sp,
+    ),
+)
